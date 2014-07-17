e81738497c09dd6a061279fd5f35bd3fd26aa029
Tommy <tomrom95@gmail.com>
2014-07-16 22:39:52 -0400

    finished test app, more to come

---
+++ app/models/micropost.rb
@@ -0,0 +1,5 @@
+class Micropost < ActiveRecord::Base
+	belongs_to :user
+	validates :content, length: { maximum: 140}
+	validates :user_id, numericality: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+	validates :name, :email, presence: true, uniqueness: true
+	has_many :microposts
 end
